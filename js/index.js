f37c0ba4c4eb0a760967fb86b631fc4788ff04ca
Beadeavila <beadeavila10@gmail.com>
2022-11-21 09:27:55 +0100

    clean js code

--- js/index.js
+++ js/index.js
@@ -1,15 +1,10 @@
 const initialList = [];
 const deletedList = [];
-
-
 function removeElementFromList() {
     const length = initialList.length;
     const elementToDelete = (Math.random() * length);
     const deleted = initialList.splice(elementToDelete, 1);
     deletedList.unshift(deleted[0])
-    /* console.log(initialList)
-    console.log(deletedList) */
-    console.log("-------------------------------------------------")
     if (length == 1) {
         document.getElementById("titulo").innerHTML= "Fin Del Juego";
         document.getElementById("boton").disabled = true;
@@ -19,10 +14,9 @@ function removeElementFromList() {
 }
     function init () {
         initialList.push("camila", "sierri", "alesia", "carmen", "beatriz");
-        renderLists()
+        renderList()
     }
-​
-    function renderLists() {
+    function renderList() {
         document.getElementById("list1").innerHTML = ""
         document.getElementById("list2").innerHTML = ""
         initialList.forEach(name => {
@@ -32,7 +26,6 @@ function removeElementFromList() {
             divName.className = "names";
             document.getElementById("list1").append(divName);
         })
-​
         deletedList.forEach(name => {
             const divName = document.createElement("div");
             divName.id = name;
@@ -41,7 +34,6 @@ function removeElementFromList() {
             document.getElementById("list2").append(divName);
         })
     }
-​
     init();
 
 
